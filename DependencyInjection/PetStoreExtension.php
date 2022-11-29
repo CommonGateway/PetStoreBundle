5dfb2d916413380efe8d4135e4b2729fcc40cfd0
Ruben van der Linde <ruben@conduction.nl>
2022-11-29 07:35:29 +0100

    New Plugin Stuff

--- DependencyInjection/PetStoreExtension.php
+++ DependencyInjection/PetStoreExtension.php
@@ -1,17 +1,16 @@
 <?php
-
-namespace CommonGateway\PetStoreBundle\DependencyInjection;
+namespace CommonGateway\PetStore\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class PetStoreExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
     }
 }
