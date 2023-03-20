0af54069e4b7c45b31ba410dccb50fc2fed3b571
Ruben van der Linde <ruben@conduction.nl>
2023-03-20 11:43:04 +0100

    First code cleanup

--- PetStoreBundle.php
+++ PetStoreBundle.php
@@ -1,11 +1,12 @@
 <?php
 
-// src/PetStoreBundle.php
-
 namespace CommonGateway\PetStoreBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Defining the core class
+ */
 class PetStoreBundle extends Bundle
 {
-}
+}//end class
--- Service/PetStoreService.php
+++ Service/PetStoreService.php
@@ -1,18 +1,23 @@
 <?php
 
-// src/Service/PetStoreService.php
 
 namespace CommonGateway\PetStoreBundle\Service;
 
+/**
+ * And axample service for adding bussnes logic to your class
+ */
 class PetStoreService
 {
-    /*
-     * Returns a welcoming string
+    /**
+     * An example handler that is triggerd by an action
      *
-     * @return array
+     * @param array $data A handler wil alway recieve the current data at that time
+     * @param array $configuration A handler can recieve optional configuration from an action or cronjobg
+     * @return array A handler mus ALWAYS return an array
      */
     public function petStoreHandler(array $data, array $configuration): array
     {
         return ['response' => 'Hello. Your PetStoreBundle works'];
-    }
-}
+    }//end petStoreHandler()
+
+}//end class
