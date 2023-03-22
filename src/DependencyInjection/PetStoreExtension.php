518726c9a4a4382cf0cecc56d9d0f51cc5fcb5db
Robert Zondervan <robert@conduction.nl>
2023-03-22 14:45:48 +0100

    Changes to be able to use src folder

--- src/DependencyInjection/PetStoreExtension.php
+++ src/DependencyInjection/PetStoreExtension.php
@@ -22,7 +22,7 @@ class PetStoreExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../Resources/config'));
         $loader->load('services.yaml');
     }//end load()
 }//end class
